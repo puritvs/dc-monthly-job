91eee10443b43c85b9ae573d22cbc2cd22612873
DESKTOP-KBIM3LD\purit <purit@verismart.co.th>
2025-04-24 13:34:39 +0700

    use textara to dislay job summay as raw text

--- src/components/JobRawTextDialog/page.tsx
+++ src/components/JobRawTextDialog/page.tsx
@@ -15,17 +15,32 @@ import { JobType } from "@/lib/types/jobType";
 import { PeriodType } from "@/lib/types/periodType";
 import { Separator } from "@radix-ui/react-select";
 import { format } from "date-fns";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ScrollArea } from "../ui/scroll-area";
 import { Job } from "@/lib/types/job";
 import { PenTool } from "lucide-react";
+import { Textarea } from "../ui/textarea";
+import { jobToText } from "@/lib/utils";
 
 export function JobRawTextDialog() {
   const { jobs } = useContext(JobsContext);
+  const [isCopied, setIsCopied] = useState(false);
+
   const sortByStartDate = (a: Job, b: Job) => {
     if (a.startDate === b.startDate) return 0;
     else if (a.startDate < b.startDate) return -1;
     else return 1;
+  };
+  const copy = async () => {
+    try {
+      await navigator.clipboard.writeText("");
+      setIsCopied(true);
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 2000);
+    } catch (error) {
+      console.error("Failed to copy text: ", error);
+    }
   };
   return (
     <Dialog>
@@ -41,7 +56,12 @@ export function JobRawTextDialog() {
         </DialogHeader>
 
         <Label>{JobType.CHOREOGRAPHER}</Label>
-        <ScrollArea className=" h-full  w-fit ">
+        <Textarea
+          className="w-full h-9/12 "
+          defaultValue={jobToText(jobs)}
+          // disabled={true}
+        />
+        {/* <ScrollArea className=" h-full  w-fit ">
           {jobs
             .sort((a: Job, b: Job) => sortByStartDate(a, b))
             .filter((job) => job.type === JobType.CHOREOGRAPHER)
@@ -80,7 +100,10 @@ export function JobRawTextDialog() {
                 <pre className=" ml-5 ">{job.description}</pre>
               </div>
             ))}
-        </ScrollArea>
+        </ScrollArea> */}
+        {/* <DialogFooter>
+          <Button>Copy</Button>
+        </DialogFooter> */}
       </DialogContent>
     </Dialog>
   );
