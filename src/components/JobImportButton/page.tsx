62af460dce29b00a41e8669ebbfcecc8c19c7a86
puritvs <purit@verismart.co.th>
2025-05-09 23:21:52 +0700

    added Toast when succesfully import

--- src/components/JobImportButton/page.tsx
+++ src/components/JobImportButton/page.tsx
@@ -1,13 +1,15 @@
 "use client";
-import { ChangeEvent, useRef } from "react";
+import { ChangeEvent, useEffect, useRef } from "react";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { useJobContext } from "@/contexts/jobsContext";
 import { toast } from "sonner";
+import { Job } from "@/lib/types/job";
 
 export default function JobImportButton() {
   const fileInputRef = useRef<HTMLInputElement>(null);
-  const { jobs } = useJobContext();
+  const { jobs,setJobs } = useJobContext();
+  const fileReader = new FileReader()
 
   const onFileSelect = async (e: ChangeEvent<HTMLInputElement>) => {
     var fileList: File[] =
@@ -18,12 +20,33 @@ export default function JobImportButton() {
     if (fileList.length >= 1) {
       console.log("file: ", fileList[0]);
     }
-    // await fetch('api/jobs',{method:'GET', headers: {
-    //     "Content-Type": "application/json",
-    //   }, })
-    // const result = fs.writeFile('')
-    //test 4
+    fileReader.readAsText(fileList[0])
+
+    fileReader.onloadend = (ev:ProgressEvent<FileReader>)=>{
+      
+      if(ev.target?.result){
+
+        var importedJobs:Job[] = JSON.parse(ev.target.result.toString()  );
+        console.log("imported: ", importedJobs);
+        setJobs(importedJobs);
+        toast.success("Import success", {
+          description: "current jobs are replaced",
+        });
+      }
+       
+    }
+    e.target.files = null;
+
+  
   };
+  // useEffect(()=>{
+
+  //   if(fileReader.result){
+  //     console.log(fileReader.result);
+      
+  //   }
+  // },[fileReader.result])
+
   return (
     <div className="flex space-x-1">
       <Button
